afc09429c1cc0822d48296a34f395d74ebc62ddf
abraxas678 <abraxas678@gmail.com>
2022-12-12 03:20:16 +0000

    1

--- change-hostname.sh
+++ change-hostname.sh
@@ -6,10 +6,14 @@ echo "#####################################################################"
 sudo chown abraxas: /home -R
 mkidr $HOME/tmp >/dev/null 2>/dev/null
 
-UBU_VERS=$(lsb_release -a | grep Release | sed 's/Release://' | sed 's/ //g'); 
-DIST=$(lsb_release -a | grep Distributor | sed 's/Distributor ID://' | sed 's/ //g');
-MACHINE="$DIST$UBU_VERS"
-MACHINE=$(echo $MACHINE | sed 's/ //g')
+if [[ ! -f /home/abraxas/MY_MACHINE ]]; then
+  UBU_VERS=$(lsb_release -a | grep Release | sed 's/Release://' | sed 's/ //g'); 
+  DIST=$(lsb_release -a | grep Distributor | sed 's/Distributor ID://' | sed 's/ //g');
+  MACHINE="$DIST$UBU_VERS"
+  MACHINE=$(echo $MACHINE | sed 's/ //g')
+else
+  MACHINE=$(cat /home/abraxas/MY_MACHINE)
+fi
 echo
 echo MACHINE $MACHINE
 MY_HOSTNAME=$MACHINE
