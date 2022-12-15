aa9e4322ea3bbf262bd4160a1e58ab20c777bf84
abraxas678 <abraxas678@gmail.com>
2022-12-15 20:15:58 +0000

    1

--- resilio-setup.sh
+++ resilio-setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ## https://help.resilio.com/hc/en-us/articles/206178924-Installing-Sync-package-on-Linux
+sudo systemctl disable resilio-sync
+sudo systemctl stop start resilio-sync
+
 echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
 curl -L https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add
 sudo apt-get update
