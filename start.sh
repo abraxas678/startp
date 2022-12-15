1633e11ad14d408f37dfe37667c61e3bce622178
abraxas678 <abraxas678@gmail.com>
2022-12-15 11:24:11 +0000

    1

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 cd $HOME
-echo "v0.43"
+echo "v0.44"
 echo
 [[ $(figlet -I test) != *"FIGlet Copyright"* ]] && sudo apt install figlet -y
 if [[ $(which rclone) != *"/usr/bin/rclone"* ]]; then
