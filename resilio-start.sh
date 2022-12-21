acc5d80e325e0872b8a6c434e7fb8877f146a9d7
abraxas678 <abraxas678@gmail.com>
2022-12-21 17:19:00 +0000

    1

---
+++ resilio-start.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+echo
+echo "====="
+echo "MKDIR"
+echo "====="
+mkdir /home/abraxas/docker >/dev/null 2>/dev/null
+mkdir /home/abraxas/docker/resilio >/dev/null 2>/dev/null
+mkdir /home/abraxas/docker/resilio/data >/dev/null 2>/dev/null
+mkdir /home/abraxas/resilio-moved/ >/dev/null 2>/dev/null
+echo
+echo "=============="
+echo "INSTALL DOCKER"
+echo "=============="
+sudo apt install docker.io -y
+echo
+echo "=============="
+echo "DOCKER WITHOUT SUDO"
+echo "=============="
+sudo groupadd docker
+sudo usermod -aG docker $USER &
+newgrp docker &
+
+echo
+echo "==========================="
+echo "MOVE RESILIO TARGET FOLDERS"
+echo "==========================="
+
+for line in $(cat resilio-folder.dat); do
+  mv /home/abraxas/$line /home/abraxas/resilio-moved/
+done
+
+echo
+echo "====================="
+echo "CREATE DOCKER-COMPOSE"
+echo "====================="
+
+echo "version: '3.3'" >docker-compose.yaml
+echo "services:" >>docker-compose.yaml
+echo "    sync:" >>docker-compose.yaml
+echo "        container_name: resilio" >>docker-compose.yaml
+echo "        ports:" >>docker-compose.yaml
+echo "            - '0.0.0.0:8888:8888'" >>docker-compose.yaml
+echo "            - 55555" >>docker-compose.yaml
+echo "        volumes:" >>docker-compose.yaml
+echo "            - '/home/abraxas/docker/resilio/data:/mnt/sync'" >>docker-compose.yaml
+echo "            - '/home/abraxas:/mnt/mounted_folders/home/abraxas'" >>docker-compose.yaml
+echo "        restart: on-failure" >>docker-compose.yaml
+echo "        image: resilio/sync" >>docker-compose.yaml
+
+echo
+echo "===================="
+echo "START DOCKER-COMPOSE"
+echo "===================="
+
+sudo docker-compose up -d
+
+echo
+echo "==========="
+echo "GET IP ADDR"
+echo "==========="
+
+sudo apt-get install dnsutils 
+echo
+echo "your ip:"
+dig +short myip.opendns.com @resolver1.opendns.com  
+echo
+echo now cf tunnel to here to access gui	 
+echo
+echo "/mnt/mounted_folders/home/abraxas"
+exit
+/usr/bin/docker run -d --name resilio \
+           -p 0.0.0.0:8888:8888 \
+           -p 55555 \
+           -v /home/abraxas/docker/resilio/data:/mnt/sync \
+           -v /home/abraxas:/mnt/mounted_folders/home/abraxas \
+           --restart on-failure \
+           resilio/sync
