bd05c31f2f0c8cf3783ebdb09c4966b50b6cbc69
abraxas678 <abraxas678@gmail.com>
2023-01-07 02:32:29 +0000

    1

--- machine_name.sh
+++ machine_name.sh
@@ -7,7 +7,6 @@ echo "                                                      machine_name.sh"
 echo
 echo
 [[ $(ls /mnt/c/MOUNT_CHECK | wc -l) = "0" ]] && WSL=0 || WSL=1
-echo; echo "WSL= $WSL"
 sleep 1
 if [[ ! -f /home/MY_MACHINE ]]; then 
   read -p "/home/MY_MACHINE does not exit. Create it? (y/n)" -n 1 MACH 
@@ -26,10 +25,8 @@ else
    MACHINE=$(cat /home/MY_MACHINE)
 fi
 MY_HOSTNAME=$MACHINE
-echo
-echo "SOLL-NAME MACHINE=  $MACHINE"
-echo
-sleep 2
+echo; echo "WSL= $WSL"
+echo; echo "SOLL-NAME MACHINE=  $MACHINE"
 
 if [[ $WSL = "1" ]]; then
 if [[ ! -f /etc/wsl.conf ]]; then
@@ -38,12 +35,20 @@ if [[ ! -f /etc/wsl.conf ]]; then
   sudo touch /etc/wsl.conf
   curl -L https://raw.githubusercontent.com/abraxas678/startp/main/wsl.conf -o wsl.conf
   sudo cp wsl.conf /etc/
+  if [[ $(cat /etc/wsl.conf) != *"hostname ="* ]]; then
+    sudo echo "[network]" >>/etc/wsl.conf
+    sudo echo "hostname = $MY_HOSTNAME" >>/etc/wsl.conf
+    sudo echo "generateHosts = false" >>/etc/wsl.conf
+  fi
   echo 
   echo "wsl.conf:"
   echo "========="
   echo
+  echo "================="
   echo cat /etc/wsl.conf
+  echo "================="
   sudo cat /etc/wsl.conf
+  echo "-----------------"
   echo
 #  read -p BUTTON15 -t 15 me 
 fi
